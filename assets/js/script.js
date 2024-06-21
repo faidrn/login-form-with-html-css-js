b33dd80ab8f7a26afdfd0400cb54faa61a00fb56
Fredy Izquierdo <m3talmaniaco@gmail.com>
2024-06-22 00:57:36 +0200

    Coding javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const neonText = document.querySelector('.neon-text');
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault(); //Evita el envío del formulario
 
-    setInterval(() => {
-        neonText.style.visibility = (neonText.style.visibility === 'hidden') ? 'visible' : 'hidden';
-    }, 1200);
+    let username = event.target.username.value;
+    let password = event.target.password.value;
+
+    //Validar entrada (esto es solo un ejemplo básico)
+    if (username === "usuario" && password == "contraseña"){
+        alert('Inicio de sesión exitoso');
+    } else{
+        alert('Usuario o contraseña incorrectos');
+    }
+
+    //Aquí se puede agregar una lógica para animaciones adicionales si se desea
 });
